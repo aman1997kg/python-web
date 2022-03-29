02d78554ec99c4ceb794e07c4d6fdf0557ab06a4
aman040997 <amanbolotbekuulu040997@gmail.com>
2022-03-29 22:46:21 +0600

    Решено некоторые задачи

--- python_script/dz/task_manager/dz_1_pohod_v_kino.py
+++ python_script/dz/task_manager/dz_1_pohod_v_kino.py
@@ -2,8 +2,15 @@
 Жанр или ужасы, или детектив, или комедии. Время от 21, если билет стоит меньше 500, иначе билет дешевле 300 ''''''''''''''''''' Функция должна возвращать TRUE или FALSE в зависимости от вводимых данных. Вы должны правильно соблюсти все условия. Жанры пишите следующим образом: comedy, detective, horror
 Условные выражения
 
-
 def func(genre: str, time: int, price: float):
+    if genre == "comedy" or genre == "detective" or genre == "horror":
+        if time > 21 and price = 500 or time < 21 and price < 300:
+            return True
+        else:
+            return False 
+    else:
+        return False
+
 '''
 
 genre = str(input())
@@ -11,11 +18,9 @@ time = int(input())
 price = float(input())
 
 if genre == "comedy" or genre == "detective" or genre == "horror":
-    if time >= 21:
-        if price < 500:
-            if price < 300:
-                print(True)
-            else:
-                print(False)
+    if time >= 21 and price <= 500 or time <= 21 and price <= 300:
+        print(True)
+    else:
+        print(False)
 else:
     print(False)
