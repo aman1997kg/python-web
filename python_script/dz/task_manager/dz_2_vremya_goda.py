69e21152b652f6adf8699a48f7d2ea766bb9934b
aman1997kg <amanbolotbekuulu1997kg@gmail.com>
2022-04-12 15:32:24 +0600

    dz_2_izm-no

--- python_script/dz/task_manager/dz_2_vremya_goda.py
+++ python_script/dz/task_manager/dz_2_vremya_goda.py
@@ -1,4 +1,6 @@
-months = ['1','2','3','4','5','6','7','8','9','10','11','12']
+""" Написать функцию season, принимающую 1 аргумент — номер месяца (от 1 до 12), и возвращающую время года, к которому этот месяц принадлежит (зима, весна, лето или осень). """
+
+months = ['4'] #['1','2','3','4','5','6','7','8','9','10','11','12']
 
 spring = dict()
 summer = dict()
@@ -25,9 +27,56 @@ winter['2'] = 'Февраль'
 for month in months:
     if month in spring:
         print('Весна')
-    elif month in summer:
+    else:
+        if month in summer:
             print('Лето')
+        else:
             if month in autumn:
                 print('Осень')
+            else:
                 if month in winter:
                     print('Зима')
+                else:
+                    print('Не правильный ввод!')
+
+
+'''
+
+def season(month):
+    
+    spring = dict()
+    summer = dict()
+    autumn = dict()
+    winter = dict()
+ 
+    spring['3'] = 'Март'
+    spring['4'] = 'Апрель'
+    spring['5'] = 'Май'
+ 
+    summer['6'] = 'Июнь'
+    summer['7'] = 'Июль'
+    summer['8'] = 'Август'
+     
+    autumn['9'] = 'Сентябрь'
+    autumn['10'] = 'Октябрь'
+    autumn['11'] = 'Ноябрь'
+     
+    winter['12'] = 'Декабрь'
+    winter['1'] = 'Январь'
+    winter['2'] = 'Февраль'
+    
+    for month in months:
+        if month in spring:
+            return 'весна'
+        else:
+            if month in summer:
+                return 'лето'
+            else:
+                if month in autumn:
+                    return 'осень'
+                else:
+                    if month in winter:
+                        return 'зима'
+                    else:
+                        return 'Не правильный ввод!'
+'''
