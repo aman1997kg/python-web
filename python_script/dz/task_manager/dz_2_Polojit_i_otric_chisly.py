0ce075c5d4f74e110c14ccaac8b468bbfff3b859
aman1997kg <amanbolotbekuulu1997kg@gmail.com>
2022-04-14 12:29:07 +0600

    Решено некоторые задачки

--- python_script/dz/task_manager/dz_2_Polojit_i_otric_chisly.py
+++ python_script/dz/task_manager/dz_2_Polojit_i_otric_chisly.py
@@ -1,13 +1,25 @@
 """ Напишите функцию, которая принимает список, содержащий положительные и отрицательные числа. Функция должна вернуть кортеж из двух списков, один из которых содержит только положительные числа, а другой отрицательные. """
+'''
+def separate_numbers(numbers_list):
+    pos = []
+    neg = []
 
-#def separate_numbers(numbers_list):
-numbers = '1,-1,2,-2,-3,-5,-6,7,8,9'
-otr = []
-pol = []
-new_list = [int(l) for l in numbers.split(',')]
-for all_list in  new_list:
-    if all_list > 0:
-        otr = otr.append(all_list)
-    else:
-        pol = pol.append(all_list)
-print(otr, pol)
+    for i in numbers_list:
+        if i > 0:
+            pos.append(i)
+        elif i < 0:
+            neg.append(i)
+    return pos, neg
+
+numbers_list = [1, 3, -1, -2, 4, -4]
+print(separate_numbers(numbers_list))
+'''
+
+def separate_numbers(numbers_list):
+    pos = [pos for pos in numbers_list if pos > 0]
+    neg = [neg for neg in numbers_list if neg < 0]
+    return pos, neg
+
+numbers_list = [1, 3, -1, -2, 4, -4]
+
+print(separate_numbers(numbers_list))
--- python_script/dz/task_manager/dz_2_vremya_goda.py
+++ python_script/dz/task_manager/dz_2_vremya_goda.py
@@ -1,82 +1,42 @@
-""" Написать функцию season, принимающую 1 аргумент — номер месяца (от 1 до 12), и возвращающую время года, к которому этот месяц принадлежит (зима, весна, лето или осень). """
+""" Написать функцию season, принимающую 1 аргумент —
+номер месяца (от 1 до 12), и возвращающую время года,
+к которому этот месяц принадлежит (зима, весна, лето или осень). """
 
-months = ['4'] #['1','2','3','4','5','6','7','8','9','10','11','12']
-
-spring = dict()
-summer = dict()
-autumn = dict()
-winter = dict()
-
-spring['3'] = 'Март'
-spring['4'] = 'Апрель'
-spring['5'] = 'Май'
-
-summer['6'] = 'Июнь'
-summer['7'] = 'Июль'
-summer['8'] = 'Август'
-
-autumn['9'] = 'Сентябрь'
-autumn['10'] = 'Октябрь'
-autumn['11'] = 'Ноябрь'
-
-winter['12'] = 'Декабрь'
-winter['1'] = 'Январь'
-winter['2'] = 'Февраль'
-
-
-for month in months:
-    if month in spring:
-        print('Весна')
-    else:
-        if month in summer:
-            print('Лето')
-        else:
-            if month in autumn:
-                print('Осень')
-            else:
-                if month in winter:
-                    print('Зима')
-                else:
-                    print('Не правильный ввод!')
-
-
-'''
 
 def season(month):
-    
     spring = dict()
     summer = dict()
     autumn = dict()
     winter = dict()
- 
+
     spring['3'] = 'Март'
     spring['4'] = 'Апрель'
     spring['5'] = 'Май'
- 
+
     summer['6'] = 'Июнь'
     summer['7'] = 'Июль'
     summer['8'] = 'Август'
-     
+
     autumn['9'] = 'Сентябрь'
     autumn['10'] = 'Октябрь'
     autumn['11'] = 'Ноябрь'
-     
+
     winter['12'] = 'Декабрь'
     winter['1'] = 'Январь'
     winter['2'] = 'Февраль'
-    
-    for month in months:
-        if month in spring:
-            return 'весна'
+
+    if str(month) in spring:
+        return 'весна'
+    else:
+        if str(month) in summer:
+            return 'лето'
         else:
-            if month in summer:
-                return 'лето'
+            if str(month) in autumn:
+               return 'осень'
             else:
-                if month in autumn:
-                    return 'осень'
+                if str(month) in winter:
+                   return 'зима'
                 else:
-                    if month in winter:
-                        return 'зима'
-                    else:
-                        return 'Не правильный ввод!'
-'''
+                   return 'Не правильный ввод!'
+
+print(season(7))
