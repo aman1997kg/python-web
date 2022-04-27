e1eb39449d424e00f3e950167ef89bcfba4a9603
aman1997kg <amanbolotbekuulu1997kg@gmail.com>
2022-04-27 16:31:18 +0600

    Решено задание Student

--- python_script/dz/task_manager/dz_2_Student.py
+++ python_script/dz/task_manager/dz_2_Student.py
@@ -3,30 +3,43 @@
 name, lastname, department, year_of_entrance.
 Добавьте метод get_student_info, который возвращает имя, фамилию,
 год поступления и факультет в отформатированном виде:
-“Вася Иванов поступил в 2017 г. на факультет: программирование.'"""
+“Вася Иванов поступил в 2017 г. на факультет: программирование.'
+
+class Student:
+    def __init__(self, name, lastname, department, year_of_entrance):
+        self.name = name
+        self.lastname = lastname
+        self.department = department
+        self.year_of_entrance = year_of_entrance
+
+
+    def get_student_info(self):
+        return f'{self.name} {self.lastname} поступил в {self.year_of_entrance} г. на факультет: {self.department}.'
+"""
 '''
 
 
 class Student:
-    def __init__(self, name, lastname, department, year_of_enterance):
-        self.__name = name
-        self.__lastname = lastname
-        self.__department = department
-        self.__year_of_enterance = year_of_enterance
+    def __init__(self, name, lastname, department, year_of_entrance):
+        self.name = name
+        self.lastname = lastname
+        self.department = department
+        self.year_of_entrance = year_of_entrance
 
         self.get_info()
-
     def get_info(self):
-        self.__name = str(input())
-        self.__lastname = str(input())
-        self.__department = str(input())
-        self.__year_of_enterance = int(input())
+        self.name = str(input())
+        self.lastname = str(input())
+        self.department = str(input())
+        self.year_of_enterance = int(input())
 
 
-    #@classmethod
+
     def get_student_info(self):
-        return f"{self.__name} {self.__lastname} поступил в {self.__year_of_enterance} г. на факультет: {self.__department}."
+        return f'{self.name} {self.lastname} поступил в {self.year_of_entrance} г. на факультет: {self.department}.'
 
-st = Student('Вася', 'Иванова','программирование', 2017)
-# st.get_student_info()
+
+
+st = Student(name=None, lastname=None, year_of_entrance=None, department=None)
+
 print(st.get_student_info())
