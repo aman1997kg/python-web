f42aceeb4eeefe57e6c6d0acde136392efec4d8e
aman1997kg <amanbolotbekuulu1997kg@gmail.com>
2022-04-27 16:51:25 +0600

    введены изминении

--- python_script/dz/task_manager/dz_2_Bus_class.py
+++ python_script/dz/task_manager/dz_2_Bus_class.py
@@ -20,14 +20,23 @@ class Vehicle:
         self.mileage = mileage
 
     def seating_capacity(self, capacity):
-        super().
-        return f"The seating capacity of a {self.name} is {capacity} passengers"
+        return f'The seating capacity of a {self.name} is {capacity} passengers'
+
+
 
 class Bus(Vehicle):
     capacity = 50
+
+    # def __init__(self, name, max_speed, mileage):
+    #     super(Bus, self).__init__(name, max_speed, mileage)
+    def seating_capacity(self, capacity):
+        return f'The seating capacity of a {self.name} is {capacity} passengers'
+
     
 vehicle = Vehicle('Toyota', 200, 20)
 vehicle.name = 'BMW'
 print(vehicle.seating_capacity(10))
 
+bus = Bus('Mers', 20, 21)
+print(bus.seating_capacity(20))
 
